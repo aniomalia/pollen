d5a2906c16f7160bf0beae283848282b2e17b646
Raoul Simionas <raulsimionas@gmail.com>
2020-05-30 19:14:55 +0300

    Added buttons to placeholders

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,14 @@ const copy = {
     lorem: {
         title: 'Vivamus mattis urna non varius',
         description: 'Nunc ut ante at elit tempus lobortis vel nec tortor non libero convallis.',
-        paragraph: 'Integer nec porta sapien. Proin posuere metus mi, sit amet commodo lorem feugiat id. Donec faucibus accumsan nisi, vitae pretium ex commodo vel. Morbi sed odio mollis, sagittis eros vel, rutrum mauris. Fusce quis sapien.'
+        paragraph: 'Integer nec porta sapien. Proin posuere metus mi, sit amet commodo lorem feugiat id. Donec faucibus accumsan nisi, vitae pretium ex commodo vel. Morbi sed odio mollis, sagittis eros vel, rutrum mauris. Fusce quis sapien.',
+        three: `Vestibulum lorem tellus, aliquet ac velit sed, convallis feugiat nulla. Donec ac mattis sapien. Nullam a ipsum eu sem tristique aliquam in sed urna. Sed nunc ligula, tincidunt vitae libero ut, dictum accumsan dui. Vivamus sapien enim, ultrices eu nisl id, ultricies fermentum urna. Morbi mollis mi ut mattis consequat. Integer malesuada nibh at lacus tempus tincidunt. Nulla eget aliquam nibh, non euismod quam. Sed a aliquam tellus, ut auctor mauris.
+
+Donec efficitur justo eget sapien congue, at imperdiet orci vestibulum.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Interdum et malesuada fames ac ante ipsum primis in faucibus.Proin faucibus justo ut semper finibus.Vestibulum pretium augue non erat imperdiet, id sodales nulla posuere.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Nunc sed fringilla libero.Vivamus lacus est, mollis ac turpis ac, condimentum consectetur risus.Suspendisse at sagittis eros.Integer facilisis enim vitae lectus placerat, quis sagittis velit iaculis.
+
+Curabitur in porta sapien, sit amet hendrerit neque.Fusce elementum leo mi, vel maximus purus ullamcorper nec.Fusce tempor non eros imperdiet posuere.Curabitur viverra congue magna, quis facilisis nibh dapibus vitae.Aenean rhoncus nisi metus, in fermentum nunc eleifend at.Aenean vulputate auctor volutpat.Ut eu scelerisque tellus, ac viverra ipsum.Donec viverra suscipit eleifend.Nullam laoreet tellus vitae libero rutrum, ullamcorper luctus ligula vulputate.Cras rutrum orci arcu.Integer nec dui vel dui finibus bibendum a in augue.In hac habitasse platea dictumst.Suspendisse eu leo a enim congue facilisis quis ut mi.Quisque vulputate, nibh ut congue fringilla, tellus quam efficitur mauris, vitae condimentum justo velit vitae diam.
+
+Maecenas lobortis massa eu lacus faucibus porttitor.Donec rutrum accumsan tortor, sed pretium arcu dictum fringilla.Pellentesque nec metus sed lacus viverra malesuada quis in lorem.Aenean aliquam a massa sed porta.Nulla.`
     },
     pollen: {
 
